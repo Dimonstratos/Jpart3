2b7d48e56014c3d2175c94b449e0319abbcfcdda
Demitros <serrasalmus.80@gmail.com>
2023-05-12 11:34:20 +0500

    src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 16145; // стоимость билета
+
+        int bonus = 20; // стоимость одной мили
+
+        System.out.println("Поздравляем! Вам начислены бонусные мили:");
+        System.out.println((ticket / bonus) + "миль");
+    }
+}
